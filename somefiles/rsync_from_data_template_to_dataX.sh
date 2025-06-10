496a0e4d6efc0376ad6a0ee25bdc34c70efa9f9a
huangsen365 <huangsen365@gmail.com>
2025-06-10 22:44:54 +0800

    Update rsync_from_data_template_to_dataX.sh

--- somefiles/rsync_from_data_template_to_dataX.sh
+++ somefiles/rsync_from_data_template_to_dataX.sh
@@ -1,4 +1,4 @@
-# wget https://raw-githubusercontent-com-huangsen365.wansio.com/huangsen365/centos7-remi-php/master/somefiles/rsync_from_data_template_to_dataX.sh -O /root/rsync_from_data_template_to_dataX.sh
+# curl -fsSL https://raw.githubusercontent.com/huangsen365/almalinux9-init-systemd-remi-php-fpm-httpd/refs/heads/main/somefiles/rsync_from_data_template_to_dataX.sh -o /root/rsync_from_data_template_to_dataX.sh
 if [ -n "$1" ]; then
     rsync -av /root/data_template/* /root/"$1"/
 else
