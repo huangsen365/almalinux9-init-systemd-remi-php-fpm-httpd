e8fbd352717d09615fddb2d1b995b009acf3436e
huangsen365 <huangsen365@gmail.com>
2025-06-13 18:17:10 +0800

    Update rsync_from_data_template_to_dataX.sh

--- somefiles/rsync_from_data_template_to_dataX.sh
+++ somefiles/rsync_from_data_template_to_dataX.sh
@@ -1,4 +1,4 @@
-# curl -fsSL https://raw-githubusercontent-com.wansio.com/huangsen365/almalinux9-init-systemd-remi-php-fpm-httpd/refs/heads/main/somefiles/rsync_from_data_template_to_dataX.sh -o /root/rsync_from_data_template_to_dataX.sh
+# curl -fsSL https://raw-githubusercontent-com.jiasu.yunbiyun.com/huangsen365/almalinux9-init-systemd-remi-php-fpm-httpd/refs/heads/main/somefiles/rsync_from_data_template_to_dataX.sh -o /root/rsync_from_data_template_to_dataX.sh
 # curl -fsSL https://raw.githubusercontent.com/huangsen365/almalinux9-init-systemd-remi-php-fpm-httpd/refs/heads/main/somefiles/rsync_from_data_template_to_dataX.sh -o /root/rsync_from_data_template_to_dataX.sh
 if [ -n "$1" ]; then
     rsync -av /root/data_template/* /root/"$1"/
